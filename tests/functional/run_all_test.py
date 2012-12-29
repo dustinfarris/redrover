aa18de235586668c8b058e3f72e22e3c9dadf365
Dustin Farris <dustin@dustinfarris.com>
2012-12-29 15:32:18 -0500

    Mental note, Nose does in fact need __init__.py to exist for test discovery.

--- tests/functional/run_all_test.py
+++ tests/functional/run_all_test.py
@@ -8,14 +8,13 @@ class RunAllTest(TestCase):
 
   @classmethod
   def setUpClass(cls):
-    cls.command = ("python {example_dir}/manage.py test " +
+    command = ("python {example_dir}/manage.py test " +
                "--where={example_dir}").format(
                  example_dir=os.environ['EXAMPLE_DIR'])
-    args = shlex.split(cls.command)
+    args = shlex.split(command)
     p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     cls.out, cls.err = p.communicate()
 
   def test_output(self):
     """Tests should run and return proper output to stdout/stderr."""
-
-    self.assertIn(self.command, self.err)
+    self.assertIn("1 test passed", self.err)
