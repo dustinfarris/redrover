17d0f33524babc4878a5fa1e4f87021cb4a23a38
Dustin Farris <dustin@dustinfarris.com>
2012-12-29 15:19:24 -0500

    Debugging Travis

--- tests/functional/run_all_test.py
+++ tests/functional/run_all_test.py
@@ -8,13 +8,14 @@ class RunAllTest(TestCase):
 
   @classmethod
   def setUpClass(cls):
-    command = ("python {example_dir}/manage.py test " +
+    cls.command = ("python {example_dir}/manage.py test " +
                "--where={example_dir}").format(
                  example_dir=os.environ['EXAMPLE_DIR'])
-    args = shlex.split(command)
+    args = shlex.split(cls.command)
     p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     cls.out, cls.err = p.communicate()
 
   def test_output(self):
     """Tests should run and return proper output to stdout/stderr."""
-    self.assertIn("1 test passed", self.err)
+
+    self.assertIn(self.command, self.err)
