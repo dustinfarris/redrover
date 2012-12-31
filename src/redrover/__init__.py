833c576ef48afa33dd9626fcf11418d3c990ce83
Dustin Farris <dustin@dustinfarris.com>
2012-12-30 22:13:47 -0500

    init update

--- src/redrover/__init__.py
+++ src/redrover/__init__.py
@@ -1,10 +1,12 @@
 from assertions import BeAssertion as be, BeValidAssertion as be_valid, \
-  EqualAssertion as equal, RespondToAssertion as respond_to
-from base import RedRoverTest
+  EqualAssertion as equal, HaveTextAssertion as have_text, \
+  RespondToAssertion as respond_to \
+
+from base import RedRoverLiveTest, RedRoverTest
 from runner import RedRoverRunner
 from subject import describe
 
 
 __all__ = [
-  'RedRoverRunner', 'RedRoverTest', 'describe', 'be', 'be_valid', 'equal',
-  'respond_to']
+  'RedRoverRunner', 'RedRoverLiveTest', 'RedRoverTest', 'describe', 'be',
+  'be_valid', 'equal', 'have_text', 'respond_to']
