230998574c24614f0887046a287bac90f1ab7d17
Dustin Farris <dustin@dustinfarris.com>
2013-01-01 21:20:32 -0500

    Use regex for the formatter

--- tests/unit/_fmt_traceback_test.py
+++ tests/unit/_fmt_traceback_test.py
@@ -10,15 +10,25 @@ class FmtTracebackTest(TestCase):
   def setUpClass(cls):
     cls.red_rover_filter = RedRoverFilter()
 
-  def test_format_remove(self):
-    """It should remove any instance of 'redrover/case.py'."""
+  def test_format_remove_subject_py(self):
+    """It should remove any instance of 'redrover/subject.py'."""
     try:
-      assert 'redrover/subject.py' in '***'
+      assert '  mystring redrover/subject.py' in '***'
     except:
       pass
     tb = sys.exc_info()[2]
     formatted_traceback = self.red_rover_filter._fmt_traceback(tb)
     self.assertNotIn('redrover/subject.py', formatted_traceback)
+
+  def test_format_remove_base_py(self):
+    """It should remove any instance of 'redrover/base.py'."""
+    try:
+      assert 'something/redrover/base.py' in '***'
+    except:
+      pass
+    tb = sys.exc_info()[2]
+    formatted_traceback = self.red_rover_filter._fmt_traceback(tb)
+    self.assertNotIn('redrover/base.py', formatted_traceback)
 
   def test_format_preserve(self):
     """It should keep everything else."""
