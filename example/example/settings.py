8a6f84bedff38bd54a4ba3171aa6bf057bc03513
Dustin Farris <dustin@dustinfarris.com>
2013-01-03 22:13:03 -0500

    Poking around with sqlite

--- example/example/settings.py
+++ example/example/settings.py
@@ -15,11 +15,8 @@ TEMPLATE_DEBUG = DEBUG
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': ':memory:',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'NAME': os.path.join(PROJECT_ROOT, 'example.db'),                      # Or path to database file if using sqlite3.
+        'TEST_NAME': ':memory:',
     }
 }
 
